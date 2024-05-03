786aba29027b34051cf823eccafb4867e9e5758c
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2024-05-03 11:54:20 +0600

    update product page and admin pannel

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import SummaryApi from '../common';
 import { setUserDetails } from '../store/userSlice';
 import { toast } from 'react-toastify';
+import ROLE from '../common/role';
 
 export const Header = () => {
     const user = useSelector(state => state?.user?.user)
@@ -55,20 +56,28 @@ export const Header = () => {
 
                 <div className='flex items-center gap-7'>
                     <div className='relative flex justify-center'>
-                        <div className='text-3xl cursor-pointer relative flex justify-center' onClick={() => setMenuDisplay(preve => !preve)}>
-                            {
-                                user?.profilePic ? (
-                                    <img src={user?.profilePic} className='w-10 h-10 rounded-full' alt={user?.name} />
-                                ) : (
-                                    <FaRegCircleUser />
-                                )
-                            }
-                        </div>
+                        {
+                            user?._id && (
+                                <div className='text-3xl cursor-pointer relative flex justify-center' onClick={() => setMenuDisplay(preve => !preve)}>
+                                    {
+                                        user?.profilePic ? (
+                                            <img src={user?.profilePic} className='w-10 h-10 rounded-full' alt={user?.name} />
+                                        ) : (
+                                            <FaRegCircleUser />
+                                        )
+                                    }
+                                </div>
+                            )
+                        }
                         {
                             menuDisplay && (
                                 <div className='absolute bg-white bottom-0 top-11 h-fit p-2 shadow-lg rounded'>
                                     <nav>
-                                        <Link to={"/admin-panel"} className='whitespace-nowrap hidden md:block hover:bg-slate-100 p-2' onClick={() => setMenuDisplay(preve => !preve)}>Admin Panel</Link>
+                                        {
+                                            user?.role === ROLE.ADMIN && (
+                                                <Link to={"/admin-panel/all-products"} className='whitespace-nowrap hidden md:block hover:bg-slate-100 p-2' onClick={() => setMenuDisplay(preve => !preve)}>Admin Panel</Link>
+                                            )
+                                        }
                                     </nav>
                                 </div>
                             )
