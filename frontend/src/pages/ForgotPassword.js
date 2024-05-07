2c35a71644271a1f841ace843d5633268284ba7d
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2024-05-07 11:06:48 +0600

    backend server update and deploy render

--- frontend/src/pages/ForgotPassword.js
+++ frontend/src/pages/ForgotPassword.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 function ForgotPassword() {
     return (
-        <div>ForgotPassword</div>
+        <>
+            <section>
+                <div className="form_data">
+                    <div className="form_heading">
+                        <h1>Enter Your NEW Password</h1>
+                    </div>
+                    <form>
+                        <div className="form_input">
+                            <label htmlFor="password">New password</label>
+                            <input type="password" name="password" id="password" placeholder='Enter Your new password' />
+                        </div>
+
+                        <button className='btn'>Send</button>
+                    </form>
+                </div>
+            </section>
+        </>
     )
 }
 
